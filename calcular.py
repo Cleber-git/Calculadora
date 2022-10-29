bca88ec0e0a308b1edb74fd63e072710b246b57b
cleber-git <cleber192875@gmail.com>
2022-10-29 15:41:50 -0300

    calcular.py
    código de cálculo

---
+++ calcular.py
@@ -0,0 +1,7 @@
+from time import sleep
+num = int(input('Digite o número desejado para o cálculo: '))
+print('-'* 30)
+for c in range(1, 11):
+    print(f'{num} x {c} = {num * c}')
+    sleep(2)
+print('-'* 30)
