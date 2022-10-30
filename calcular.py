304f35662d07d3e5890e49841741170a7afa374f
cleber-git <cleber192875@gmail.com>
2022-10-29 22:19:01 -0300

    Update calcular.py

--- calcular.py
+++ calcular.py
@@ -3,7 +3,7 @@ num = int(input('Digite o número desejado para o cálculo: '))
 print('-'* 30)
 for c in range(1, 11):
     print(f'{num} x {c} = {num * c}')
-    sleep(2)
+    sleep(1.5)
 print('-'* 30)
 
 print('fim do progranma')
