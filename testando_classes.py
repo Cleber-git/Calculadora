6e42e3d8813d3b96df1584294ae54d5c9536aa69
Cleber-git <cleber192875@gmail.com>
2022-11-17 02:44:49 -0200

    update testando_classes

--- testando_classes.py
+++ testando_classes.py
@@ -4,26 +4,29 @@ class carro:
         self.ano =  ano
         self.dono = ano
         self.velocidade = 0
-    
+        self.velalt = 0
     def ligar(self):
         self.velocidade = 10
         from time import sleep
         print('Automóvel dando partida...')
         sleep(1.5)
         print(f'Carro em movimento, {self.velocidade}km/h')
+        sleep(1.5)
 
     def frear(self):
         from time import sleep
         print('Carro freando...')
         sleep(1.5)
+        self.velocidade = 0
         print(f'A velocidade atual do carro é: {self.velocidade}km/h')
     
     def show(self):
-        print(f'O carro está em {self.velocidade}km/h')
+        print(f'O carro está marcando {self.velocidade}km/h')
 
     def acelerar(self, veloz):
         if veloz >= 0:
             self.velocidade += veloz 
+        print(f'Acelerando {veloz}km/h')
 
 
 cleber = carro('fiat', 2002, 'Cleber Neves')
