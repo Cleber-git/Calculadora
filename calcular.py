ce73664aa4e2f13d0b1017a434afde1a8ef31353
cleber-git <cleber192875@gmail.com>
2022-11-18 22:27:06 -0300

    Update calcular.py

--- calcular.py
+++ calcular.py
@@ -1,9 +1,12 @@
 from time import sleep
-num = int(input('Digite o número desejado para o cálculo: '))
-print('-'* 30)
-for c in range(1, 11):
-    print(f'{num} x {c} = {num * c}')
-    sleep(2)
-print('-'* 30)
+while True:
+    num = float(input('Digite o número desejado para o cálculo: '))
+    if num == 0:
+        break
+    print('-'* 30)
+    for c in range(1, 11):
+        print(f'{num} x {c} = {num * c}')
+        sleep(2)
+    print('-'* 30)
 
 print('fim do programa')
