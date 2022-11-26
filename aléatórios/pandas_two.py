478749bf22932515bc9d83ff0696d41de1871853
cleber-git <cleber192875@gmail.com>
2022-11-26 13:41:23 -0300

    atualizações no pandas dois

--- aléatórios/pandas_two.py
+++ aléatórios/pandas_two.py
@@ -18,7 +18,13 @@ vendas_df = vendas_df.loc[:, ['ID Loja', 'Produto', 'Valor Unitário']]
 # print(vendas_df)
 
 #  deletar coluna específica 
-vendas_df = vendas_df.drop('ID Loja', axis=1)
-print(vendas_df)
+# vendas_df = vendas_df.drop('ID Loja', axis=1)
+# print(vendas_df)
 
-# 
+# criar coluna
+vendas_df['Comissão'] = vendas_df['Valor Unitário'] * 0.05
+# print(vendas_df)
+
+# filtrando tabela
+vendas_df_norte = vendas_df.loc[vendas_df['ID Loja'] == 'Norte Shopping']
+print(vendas_df_norte)
